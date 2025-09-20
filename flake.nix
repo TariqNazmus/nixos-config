680c58c04e007df917eaf4b1bd2616e883286107
TariqNazmus <tariq.nazmus@gmail.com>
2025-09-20 12:56:17 +0000

    fix hm failing 2

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup"; # or any extension you prefer
             home-manager.users.sadat = import ./home/home.nix;
           }
         ];
